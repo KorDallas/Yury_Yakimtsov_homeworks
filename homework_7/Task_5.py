e14bf18cc379dabc34c7623b5ef3fcaa0b187134
Yury <YakimYury@hotmail.com>
2023-06-28 19:01:18 +0300

    added changes in Task_5

--- homework_7/Task_5.py
+++ homework_7/Task_5.py
@@ -1,7 +1,10 @@
 # Заполните список ста нулями, кроме первого и последнего элементов, они дожни быть равны единице.
 
 list_1 = [0 for i in range(100)]
-list_1.append(1)
-list_1.insert(0, 1)
+index_list = list_1.index(0)
+list_1[index_list] = 1
+list_1.sort()
+index_list = list_1.index(0)
+list_1[index_list] = 1
 print(list_1)  # Весь список 100 нулей, первый и последний элемент = 1
 print(len(list_1))  # длинна списка
